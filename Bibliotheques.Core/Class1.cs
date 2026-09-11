cff48eb732d6dd633d5dde0c98b2d920881dc4f8
rania bechir <raniabechir1995@gmail.com>
2026-09-11 15:03:45 -0400

    first commit

---
+++ Bibliotheques.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Bibliotheques.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Bibliotheques.Data/Bibliotheque.cs
@@ -0,0 +1,12 @@
+﻿namespace Bibliotheques.Data
+{
+    public class Bibliotheque
+    {
+        public int Id { get; set; }
+
+        public string Nom { get; set; } = "";
+
+        public string Adresse { get; set; } = "";
+
+    }
+}
